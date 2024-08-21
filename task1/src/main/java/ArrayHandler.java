bc9bc7e72fd5be6ea3b2b8ecd3eb1ef42283f7ee
Ефентьев Александр Геннадьевич <mr.efentev@mail.ru>
2024-08-22 02:16:35 +0700

    third alg done

--- task1/src/main/java/ArrayHandler.java
+++ task1/src/main/java/ArrayHandler.java
@@ -4,20 +4,36 @@ import java.util.Random;
 
 @Slf4j
 public class ArrayHandler {
-    private final int arraySize;
+//    private final int arraySize;
+//    private final int bound;
 
-    private Random random = new Random();
+//    private Random random = new Random();
 
-    public ArrayHandler(int arraySize) {
-        this.arraySize = arraySize;
-    }
+//    public ArrayHandler(int arraySize, int bound) {
+//        this.arraySize = arraySize;
+//        this.bound = bound;
+//    }
 
-    public int[] fillInArrayAndGet() {
+    public static int[] fillInArrayAndGet(int arraySize, int bound) {
+
+        Random random = new Random();
 
         int[] array = new int[arraySize];
 
         for (int i = 0; i < arraySize; i++) {
-            array[i] = random.nextInt(10);
+            array[i] = random.nextInt(bound) + 1;
+        }
+        return array;
+    }
+
+    public static int[] fillInArrayAndGet(int arraySize) {
+
+        Random random = new Random();
+
+        int[] array = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt();
             log.info(String.valueOf(array[i]));
         }
         return array;
--- task1/src/main/java/Main.java
+++ task1/src/main/java/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        ArrayHandler arrayHandler = new ArrayHandler(100);
-        int[] array = arrayHandler.fillInArrayAndGet();
-        Counter counter = new Counter();
-        FirstAlgorithmHandler firstAlgHandler = new FirstAlgorithmHandler(array, counter);
-        firstAlgHandler.handleArrayByFirstAlgorithm();
-        firstAlgHandler.printResults();
+//        int[] array = ArrayHandler.fillInArrayAndGet(100,10);
+//        Counter counter = new Counter();
+//        FirstAlgorithmHandler firstAlgHandler = new FirstAlgorithmHandler(array, counter);
+//        firstAlgHandler.handleArrayByFirstAlgorithm();
+//        firstAlgHandler.printResults();
+
+        GameHandler gameHandler = new GameHandler(50);
+        gameHandler.handle();
     }
 
 }
